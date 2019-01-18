1aff83bfec5714c8aaedf5d4fb7dcf545451112f
lucassasso <contato@lucassasso.com.br>
2019-01-17 23:24:07 -0200

    pageadmin

--- index.php
+++ index.php
@@ -1,22 +1,31 @@
 <?php 
-
+ 
 require_once("vendor/autoload.php");
-
+ 
 use \Slim\Slim;
 use \Hcode\Page;
-
-$app = new Slim();
-
+use \Hcode\PageAdmin;
+ 
+$app = new \Slim\Slim();
+ 
 $app->config('debug', true);
-
+ 
 $app->get('/', function() {
-	
-	$page = new Page();
-
-	$page->setTpl("index");
-	
+    
+    $page = new Page();
+ 
+    $page->setTpl("index");
+ 
 });
-
+ 
+$app->get('/admin/', function() {
+    
+    $page = new PageAdmin();
+ 
+    $page->setTpl("index");
+ 
+});
+ 
 $app->run();
-
+ 
  ?>
